844bc61e96752b867430eefc7c330b9145f04255
迷渡 <justjavac@gmail.com>
2022-07-25 21:13:58 +0800

    upgrade std@0.149

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.148.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.149.0/http/server.ts";
 
 async function handleRequest(request: Request) {
   const { pathname } = new URL(request.url);
