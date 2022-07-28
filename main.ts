b8e48362de14ee9db63ae95a9e4eba2cfc41e5e8
迷渡 <justjavac@gmail.com>
2022-07-28 17:18:05 +0800

    add gfm
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- main.ts
+++ main.ts
@@ -1,10 +1,43 @@
 import { serve } from "https://deno.land/std@0.149.0/http/server.ts";
+import { CSS, render } from "https://deno.land/x/gfm@0.1.22/mod.ts";
 
 async function handleRequest(request: Request) {
   const { pathname } = new URL(request.url);
 
   if (pathname === "/") {
-    return new Response(await Deno.readFile("README.md"));
+    const readme = await Deno.readTextFile("./README.md");
+    const body = render(readme);
+    const html = `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>GitHub Raw Proxy</title>
+          <style>
+            body {
+              margin: 0;
+              background-color: var(--color-canvas-default);
+              color: var(--color-fg-default);
+            }
+            main {
+              max-width: 800px;
+              margin: 0 auto;
+              padding: 2rem 1rem;
+            }
+            ${CSS}
+          </style>
+        </head>
+        <body data-color-mode="auto" data-light-theme="light" data-dark-theme="dark">
+          <main class="markdown-body">
+            ${body}
+          </main>
+        </body>
+      </html>`;
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html;charset=utf-8",
+      },
+    });
   }
 
   if (pathname === "/favicon.ico") {
